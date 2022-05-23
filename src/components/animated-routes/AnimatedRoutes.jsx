fd7fb7e51b04baf933104d902765f684798b2304
gyanbhartip <gyanbhartip@gmail.com>
2022-05-23 09:28:44 +0530

    optimizations added

--- src/components/animated-routes/AnimatedRoutes.jsx
+++ src/components/animated-routes/AnimatedRoutes.jsx
@@ -1,12 +1,12 @@
 import { AnimatePresence } from "framer-motion";
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import App from "../app/App";
 import User from "../user/User";
 
-const FinishComponent = lazy(() => import("../finish/Finish"));
-const TypeComponent = lazy(() => import("../type/Type"));
-const WorkspaceComponent = lazy(() => import("../workspace/Workspace"));
+const Finish = lazy(() => import("../finish/Finish"));
+const Type = lazy(() => import("../type/Type"));
+const Workspace = lazy(() => import("../workspace/Workspace"));
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -16,9 +16,30 @@ const AnimatedRoutes = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<App />}>
           <Route index element={<User />} />
-          <Route path="/workspace" element={<WorkspaceComponent />} />
-          <Route path="/type" element={<TypeComponent />} />
-          <Route path="/finish" element={<FinishComponent />} />
+          <Route
+            path="/workspace"
+            element={
+              <Suspense fallback={<h3>...</h3>}>
+                <Workspace />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/type"
+            element={
+              <Suspense fallback={<h3>...</h3>}>
+                <Type />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/finish"
+            element={
+              <Suspense fallback={<h3>...</h3>}>
+                <Finish />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </AnimatePresence>
